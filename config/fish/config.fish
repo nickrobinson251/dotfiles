cdba92a20ab20062b31536eb173d8a69776f6179
nick <npr251@gmail.com>
2017-09-28 10:02:54 +0100

    fish: Use conda including virtual environemnts
    Documentation for using conda virtual envs with fish here:
    https://github.com/conda/conda/blob/
    be8c08c083f4d5e05b06bd2689d2cd0d410c2ffe/
    shell/etc/fish/conf.d/conda.fish#L9-L28

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,2 +1,12 @@
 # print directory names in brighter blue
 set -gx LSCOLORS gxfxcxdxbxegedabagacad
+
+# append anaconda to path so we can use conda
+set -gx PATH /Users/npr/anaconda/bin $PATH
+
+# make using conda virtual envs possible. Start/stop with
+# '[conda] activate <env>' and '[conda] deactivate'
+source (conda info --root)/etc/fish/conf.d/conda.fish
+
+# make environment name appear on the left side of prompt
+set -gx CONDA_LEFT_PROMPT 1
